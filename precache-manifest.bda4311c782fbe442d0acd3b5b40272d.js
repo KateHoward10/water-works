ff8b016d61138bc38e4214f5ad7fc567121d5093
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-04 13:02:04 +0100

    Updates

---
+++ precache-manifest.bda4311c782fbe442d0acd3b5b40272d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299cb13948190fd0b2ff10fde7c90101",
+    "url": "/water-works/index.html"
+  },
+  {
+    "revision": "c3cf0fe6cd17d5e27d62",
+    "url": "/water-works/static/css/main.60ec79d1.chunk.css"
+  },
+  {
+    "revision": "6dd350ccadae2ec3709f",
+    "url": "/water-works/static/js/2.39abf178.chunk.js"
+  },
+  {
+    "revision": "c3cf0fe6cd17d5e27d62",
+    "url": "/water-works/static/js/main.737b1a34.chunk.js"
+  },
+  {
+    "revision": "1ae5962dd1c5e710a713",
+    "url": "/water-works/static/js/runtime-main.33e0e7a3.js"
+  },
+  {
+    "revision": "8c4867f68219183bfe90dc2b0c88f3a0",
+    "url": "/water-works/static/media/waver.8c4867f6.ttf"
+  },
+  {
+    "revision": "ce6fe022f7d1a597478eeb3dc217f38d",
+    "url": "/water-works/static/media/world_of_water.ce6fe022.ttf"
+  }
+]);
